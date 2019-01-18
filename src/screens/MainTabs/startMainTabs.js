72a0d13c54ad50b5ae3692ecbc4e5792f6997c9f
chihiro <chihiro@chihiroui-MacBook-Pro.local>
2019-01-19 01:19:59 +0900

    react-native-navigation settings

--- src/screens/MainTabs/startMainTabs.js
+++ src/screens/MainTabs/startMainTabs.js
@@ -1,10 +1,13 @@
 import { Navigation } from 'react-native-navigation';
 import Icon from 'react-native-vector-icons/Ionicons';
+import newspaperIcon from '../../assets/newspaper.svg'
+import calendarIcon from '../../assets/calendar.svg'
 
 const startTabs = () => {
     Promise.all([
         Icon.getImageSource("md-map", 30),
         Icon.getImageSource("ios-share-alt", 30)
+
     ]).then(sources => {
         Navigation.startTabBasedApp({
             tabs: [
